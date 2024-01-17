6770368cc0170cf9999a57bf6175859c72e1a4ec
Mudassar Nazir <mudassarnazir137@gmail.com>
2024-01-17 20:05:05 +0500

    Looks Perfect

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+function play1() {
+
+    var sound1 = document.getElementById("audio1").play();
+    
+}
+function play2() {
+    var sound2 = document.getElementById("audio2").play();
+}
+function play3() {
+    var sound2 = document.getElementById("audio3").play();
+}
+function play4() {
+    var sound2 = document.getElementById("audio4").play();
+}
+function play5() {
+    var sound2 = document.getElementById("audio5").play();
+}
+function play6() {
+    var sound2 = document.getElementById("audio6").play();
+}
+function play7() {
+    var sound2 = document.getElementById("audio7").play();
+}
+function play8() {
+    var sound2 = document.getElementById("audio8").play();
+}
+function play9() {
+    document.getElementById("audio9").play();
+    
+}
+
+ 
